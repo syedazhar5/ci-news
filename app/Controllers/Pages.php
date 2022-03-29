7ed245dcdc95fc4acb8e4bdac1c57b96b0a7d01e
Syed Azhar <syed.azhar9465@gmail.com>
2022-03-29 17:22:39 +0530

    Static pages added

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -10,7 +10,16 @@ class Pages extends BaseController
     }
 
     public function view($page = 'home')
-    {
-        // ...
-    }
+        {
+            if (! is_file(APPPATH . 'Views/pages/' . $page . '.php')) {
+                // Whoops, we don't have a page for that!
+                throw new \CodeIgniter\Exceptions\PageNotFoundException($page);
+            }
+        
+            $data['title'] = ucfirst($page); // Capitalize the first letter
+        
+            echo view('templates/header', $data);
+            echo view('pages/' . $page, $data);
+            echo view('templates/footer', $data);
+        }
 }
